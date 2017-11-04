18abfeba3e0f6edf13b764339194e0f805e8b4e2
bwitoradyo <bambang.witoradyo@gmail.com>
2017-11-04 15:24:12 -0400

    Create The BookItem Component

--- src/components/pages/booksList.js
+++ src/components/pages/booksList.js
@@ -2,6 +2,7 @@
 import React from "react";
 import {connect} from "react-redux";
 import {bindActionCreators} from "redux";
+import {Grid, Col, Row, Button} from "react-bootstrap";
 
 import {getBooks} from "../../actions/booksActions";
 
@@ -18,13 +19,16 @@ class BooksList extends React.Component{
           <h2>{booksArr.title}</h2>
           <h2>{booksArr.description}</h2>
           <h2>{booksArr.price}</h2>
+	  <Button bsStyle="primary">Buy now</Button>
         </div>		      
       )
     })
     return(
-      <div>
-	{booksList}
-      </div>		    
+      <Grid>
+      	<Row style={{marginTop:"15px"}}>
+	  {booksList}
+	</Row>
+      </Grid>
     ) 
   }
 }
