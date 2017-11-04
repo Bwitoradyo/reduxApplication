df75dde900ad018013b78f9f5cce18e5f61dda44
bwitoradyo <bambang.witoradyo@gmail.com>
2017-11-04 15:44:30 -0400

    Create The BooksForm Component

---
+++ src/components/pages/bookItem.js
@@ -0,0 +1,22 @@
+"use strict";
+import React from "react";
+import {Row, Col, Well, Button} from "react-bootstrap";
+
+class BookItem extends React.Component{
+  render(){
+    return(
+      <Well>
+      	<Row>
+      	  <Col xs={12}>
+      	    <h5>{this.props.title}</h5>
+      	    <p>{this.props.description}</p>
+      	    <h6>{this.props.price}</h6>
+      	    <Button bsStyle="primary"></Button>
+      	  </Col>
+      	</Row>
+      </Well>		    
+    )
+  }
+}
+
+export default BookItem;
