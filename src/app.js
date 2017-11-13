86b99a37fbfd0f19a799f75152885ebb745c755e
Bwitoradyo <bambang.witoradyo@gmail.com-MacBook-Pro.local>
2017-11-13 00:02:15 -0500

    Broke React Router

--- src/app.js
+++ src/app.js
@@ -4,6 +4,9 @@ import React from "react";
 import {render} from "react-dom";
 import {Provider} from "react-redux";
 
+// REACT ROUTER
+import {Router, Route, IndexRoute, browserHistory} from "react-router";
+
 import {applyMiddleware, createStore} from "redux";
 import logger from "redux-logger";
 
@@ -20,18 +23,26 @@ const store = createStore(reducers, middleware);
 
 
 import BooksList from "./components/pages/booksList";
-import Menu from "./components/pages/menu";
-import Footer from "./components/pages/footer";
+import Cart from "./components/pages/cart";
+import BooksForm from "./components/pages/booksForm";
+import Main from "./main";
+
+
+const Routes = (
+  <Provider store={store}>
+    <Router history={browserHistory}>
+      <Route path="/" component={Main}>
+        <IndexRoute component={BooksList}/>
+	<Route path="/admin" component={BooksForm}/>
+	<Route path="/cart" component={Cart}/>
+      </Route>
+    </Router>
+  </Provider>, document.getElementById("app")		
+  
+)
 
 render(
-  <Provider store={store}>
-    <div>
-    <Menu/>
-    <BooksList/>	
-    <Footer/>
-    </div>
-
-  </Provider>, document.getElementById("app")		
+  Routes, document.getElementById("app")
 )
 //STEP 2 create and dispatch actions
 
